f95aab44be078b93f54f824b8550b551b83c93b7
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-05-28 17:06:29 +0530

    committing changes

--- peering.tf
+++ peering.tf
@@ -18,7 +18,7 @@ resource "aws_route" "public_peering" {
     count = var.is_peering_required && var.peer_vpc_id == "" ? 1 : 0
     route_table_id            = aws_route_table.public.id
     destination_cidr_block    = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer_conn[count.index].id
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer_conn[0].id
 }
 
 #aws private route creation for peering with default vpc
@@ -26,7 +26,7 @@ resource "aws_route" "private_peering" {
     count = var.is_peering_required && var.peer_vpc_id == "" ? 1 : 0
     route_table_id            = aws_route_table.private.id
     destination_cidr_block    = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer_conn[count.index].id
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer_conn[0].id
 }
 
 #aws database route creation for peering with default vpc
@@ -34,13 +34,13 @@ resource "aws_route" "database_peering" {
     count = var.is_peering_required && var.peer_vpc_id == "" ? 1 : 0
     route_table_id            = aws_route_table.database.id
     destination_cidr_block    = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer_conn[count.index].id
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer_conn[0].id
 }
 
 #aws default VPC - route creation for peering with expense vpc
 resource "aws_route" "acceptor_peering" {
     count = var.is_peering_required && var.peer_vpc_id == "" ? 1 : 0
-    route_table_id            = data.aws_vpc.default.main_route_table_id
-    destination_cidr_block    = aws_vpc.main.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer_conn[count.index].id
+    route_table_id            = data.aws_route_table.main.id
+    destination_cidr_block    = var.vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer_conn[0].id
 }
